889d12d11948839b6db712319145cfad4bcc4bc2
rumsky <hongyu.zhao@bitmain.com>
2019-08-30 11:22:17 +0800

    add hover animation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
 import {
   useMenuState,
   useHiddenState,
@@ -16,33 +17,95 @@ const HomeWrap = styled.div`
   margin: 100px auto;
   line-height: 2;
 `;
-const StyledMenuItem = styled(MenuItem)`
-  display: block;
-  padding: 4px 6px;
-  color: #00c;
+
+const MenuContainer = styled.div`
+  display: inline-block;
+`;
+
+const StyledMenuDisclosure = styled(MenuItem)`
+  color: #09c;
+  font-size: 16px;
+  padding: 12px;
+  position: relative;
+  background: transparent;
+  border: none;
+  vertical-align: middle;
+  &:hover {
+    color: #0c2;
+  }
+  outline: none;
 `;
 const StyledMenu = styled(Menu)`
-  background-color: #fff;
-  border-radius: 4px;
+  position: absolute;
+  border-top: 2px solid #9ce;
+  padding: 10px;
+  background: #fff;
+  border-radius: 0 0 4px 4px;
+  box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.04);
+  outline: none;
+`;
 
-  padding: 8px 10px;
-  color: #00c;
+const StyledMenuItem = styled(MenuItem)`
+  white-space: nowrap;
+  display: block;
+  padding: 15px 20px;
+  border: none;
+  background: #fff;
+  border-radius: 4px;
+  &:hover {
+    box-shadow: none;
+    outline: none;
+    color: #9ce;
+  }
+  outline: none;
 `;
 
 const Home = () => {
   const menu = useMenuState();
 
+  const { opacity, scale, display } = useSpring({
+    opacity: menu.visible ? 1 : 0,
+    display: menu.visible ? 'block' : 'none',
+    scale: menu.visible ? 1 : 0,
+    onRest: menu.unstable_stopAnimation,
+    config: name => ({
+      tension: name === 'opacity' ? 250 : 300,
+      friction: 25
+    })
+  });
+
   return (
     <HomeWrap>
-      <p>Next with Reakit</p>
-      <MenuDisclosure {...menu}>Preferences</MenuDisclosure>
-      <StyledMenu {...menu} aria-label="Preferences">
-        <StyledMenuItem {...menu} disabled>
-          Extensions
-        </StyledMenuItem>
-        <MenuSeparator {...menu} />
-        <StyledMenuItem {...menu}>Keyboard shortcuts</StyledMenuItem>
-      </StyledMenu>
+      <h1>Next with Reakit</h1>
+      <MenuContainer
+        onMouseEnter={() => {
+          menu.show();
+        }}
+        onMouseLeave={() => {
+          menu.hide();
+        }}
+      >
+        <StyledMenuDisclosure {...menu}>Animated Menu</StyledMenuDisclosure>
+        <StyledMenu
+          {...menu}
+          aria-label="Preferences"
+          as={animated.div}
+          style={{
+            opacity,
+            display,
+            transformOrigin: 'top bottom',
+            transform: scale.interpolate(
+              s => `${menu.unstable_popoverStyles.transform} scaleY(${s})`
+            )
+          }}
+        >
+          <StyledMenuItem {...menu} disabled>
+            Extensions
+          </StyledMenuItem>
+          <MenuSeparator {...menu} />
+          <StyledMenuItem {...menu}>Keyboard shortcuts</StyledMenuItem>
+        </StyledMenu>
+      </MenuContainer>
     </HomeWrap>
   );
 };
